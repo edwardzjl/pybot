49f96222edbc35d5efa34e41fbe81b0985ce735d
Junlin Zhou <jlzhou@zjuici.com>
2023-09-14 14:34:09 +0800

    chore: add jupyter enterprise gateway manifest

---
+++ manifests/third-party/jupyter-enterprise-gateway/helm-template.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+VERSION="3.2.2"
+OUTPUT="jupyter-enterprise-gateway.yaml"
+NAMESPACE="jupyter"
+
+# --output-dir has some pattern
+helm template enterprise-gateway \
+  https://github.com/jupyter-server/enterprise_gateway/releases/download/v$VERSION/jupyter_enterprise_gateway_helm-$VERSION.tar.gz \
+  --namespace $NAMESPACE \
+  > $OUTPUT
