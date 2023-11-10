63e67516ca8fd95df9bf19c5b68fe33ca9d98124
Junlin Zhou <jlzhou@zjuici.com>
2023-11-10 11:26:59 +0800

    refactor: extract userid header

--- api/pybot/config.py
+++ api/pybot/config.py
@@ -6,6 +6,7 @@ class Settings(BaseSettings):
     log_level: str = "INFO"
     redis_om_url: RedisDsn = "redis://localhost:6379"
     inference_server_url: AnyHttpUrl = "http://localhost:8080"
+    user_id_header: str = "kubeflow-userid"
 
 
 settings = Settings()
