7040d7506a14adf39cb67b2005319c64781a999d
jlzhou <jameszhou2108@hotmail.com>
2023-11-13 22:13:38 +0800

    feat: add tracing llm callback

--- api/pybot/callbacks/__init__.py
+++ api/pybot/callbacks/__init__.py
@@ -1,7 +1,9 @@
 from pybot.callbacks.conversation import UpdateConversationCallbackHandler
 from pybot.callbacks.streaming import StreamingLLMCallbackHandler
+from pybot.callbacks.tracing import TracingLLMCallbackHandler
 
 __all__ = [
     "UpdateConversationCallbackHandler",
     "StreamingLLMCallbackHandler",
+    "TracingLLMCallbackHandler",
 ]
