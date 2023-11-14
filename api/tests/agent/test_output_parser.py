4e0cb21be945a17191bbb9bda42ed0bef2f81e43
Junlin Zhou <jlzhou@zjuici.com>
2023-11-14 14:55:35 +0800

    feat: a more versatile output parser

---
+++ api/tests/agent/test_output_parser.py
@@ -0,0 +1,15 @@
+import unittest
+
+from pybot.agent.output_parser import JsonOutputParser
+
+
+class TestJsonOutputParser(unittest.TestCase):
+    def test_parse(self):
+        parser = JsonOutputParser()
+        output = """ To create a dataframe in Python, you can use the pandas library. Here\'s an example of creating a dataframe with two columns, one for \'Name\' and another for \'Age\':\n\n```python\nimport pandas as pd\n\ndata = [\n    {\'Name\': \'Alice\', \'Age\': 25},\n    {\'Name\': \'Bob\', \'Age\': 30},\n    {\'Name\': \'Charlie\', \'Age\': 28}\n]\n\ndf = pd.DataFrame(data)\n```\n\nNow, the dataframe \'df\' contains the data in a structured format, which can be easily manipulated and analyzed.\n\nTo use this example in code_sandbox, you can send the following message:\n\n```\n{\n  "tool_name": "code_sandbox",\n  "tool_input": "import pandas as pd\\n\\ndata = [{\\n    \'Name\': \'Alice\', \'Age\': 25},\\n    {\\n    \'Name\': \'Bob\', \'Age\': 30},\\n    {\\n    \'Name\': \'Charlie\', \'Age\': 28}\\n]\\n\\ndf = pd.DataFrame(data)"\n}\n```\n\nCode_sandbox will execute the Python code and return the output."""
+        parsed = parser.parse(output)
+        self.assertEqual(parsed.tool, "code_sandbox")
+
+
+if __name__ == "__main__":
+    unittest.main()
