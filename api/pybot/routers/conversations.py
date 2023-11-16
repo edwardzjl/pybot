d4325545f2d058493ea031a1032bdfbbf497eec7
jlzhou <jameszhou2108@hotmail.com>
2023-11-16 21:52:52 +0800

    chore: leverage type hint on router functions

--- api/pybot/routers/conversations.py
+++ api/pybot/routers/conversations.py
@@ -23,19 +23,21 @@ router = APIRouter(
 gateway_client = GatewayClient(host=settings.jupyter_enterprise_gateway_url)
 
 
-@router.get("", response_model=list[Conversation])
-async def get_conversations(userid: Annotated[str | None, UserIdHeader()] = None):
+@router.get("")
+async def get_conversations(
+    userid: Annotated[str | None, UserIdHeader()] = None
+) -> list[Conversation]:
     convs = await ORMConversation.find(ORMConversation.owner == userid).all()
     convs.sort(key=lambda x: x.updated_at, reverse=True)
     return [Conversation(**conv.dict()) for conv in convs]
 
 
-@router.get("/{conversation_id}", response_model=ConversationDetail)
+@router.get("/{conversation_id}")
 async def get_conversation(
     conversation_id: str,
     history: Annotated[RedisChatMessageHistory, Depends(get_message_history)],
     userid: Annotated[str | None, UserIdHeader()] = None,
-):
+) -> ConversationDetail:
     conv = await ORMConversation.get(conversation_id)
     history.session_id = f"{userid}:{conversation_id}"
     return ConversationDetail(
@@ -51,8 +53,10 @@ async def get_conversation(
     )
 
 
-@router.post("", status_code=201, response_model=ConversationDetail)
-async def create_conversation(userid: Annotated[str | None, UserIdHeader()] = None):
+@router.post("", status_code=201)
+async def create_conversation(
+    userid: Annotated[str | None, UserIdHeader()] = None
+) -> ConversationDetail:
     env = {
         "KERNEL_USERNAME": userid,
         "KERNEL_VOLUME_MOUNTS": [{"name": "nfs-volume", "mountPath": "/mnt/data"}],
@@ -77,7 +81,7 @@ async def update_conversation(
     conversation_id: str,
     payload: UpdateConversation,
     userid: Annotated[str | None, UserIdHeader()] = None,
-):
+) -> None:
     conv = await ORMConversation.get(conversation_id)
     conv.title = payload.title
     await conv.save()
@@ -87,7 +91,7 @@ async def update_conversation(
 async def delete_conversation(
     conversation_id: str,
     userid: Annotated[str | None, UserIdHeader()] = None,
-):
+) -> None:
     conv = await ORMConversation.get(conversation_id)
     try:
         gateway_client.delete_kernel(conv.kernel_id)
