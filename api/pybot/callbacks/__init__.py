2bdd4ee07496a3e364c0616ae0f4ea7205012fde
jlzhou <jameszhou2108@hotmail.com>
2023-11-21 23:37:53 +0800

    feat: action message and history

--- api/pybot/callbacks/__init__.py
+++ api/pybot/callbacks/__init__.py
@@ -1,8 +1,10 @@
+from pybot.callbacks.action import AgentActionCallbackHandler
 from pybot.callbacks.conversation import UpdateConversationCallbackHandler
 from pybot.callbacks.streaming import StreamingLLMCallbackHandler
 from pybot.callbacks.tracing import TracingLLMCallbackHandler
 
 __all__ = [
+    "AgentActionCallbackHandler",
     "UpdateConversationCallbackHandler",
     "StreamingLLMCallbackHandler",
     "TracingLLMCallbackHandler",
---
+++ api/pybot/callbacks/action.py
@@ -0,0 +1,102 @@
+import json
+from typing import Any, Optional
+from uuid import UUID
+
+from fastapi import WebSocket
+from langchain.callbacks.base import AsyncCallbackHandler
+from langchain.schema import AgentAction, BaseChatMessageHistory, SystemMessage
+
+from pybot.schemas import ChatMessage
+
+
+class AgentActionCallbackHandler(AsyncCallbackHandler):
+    """Callback handler for sending tool execution information back to user."""
+
+    def __init__(
+        self,
+        websocket: WebSocket,
+        conversation_id: str,
+        history: BaseChatMessageHistory,
+    ):
+        self.websocket = websocket
+        self.conversation_id = conversation_id
+        self.history = history
+
+    async def on_agent_action(
+        self,
+        action: AgentAction,
+        *,
+        run_id: UUID,
+        parent_run_id: Optional[UUID] = None,
+        tags: Optional[list[str]] = None,
+        **kwargs: Any,
+    ) -> None:
+        """Persist 'thought' (action log) to history.
+        Action log was sent to user by streaming callback, I don't need to send it again here.
+        """
+        self.history.add_ai_message(action.log)
+
+    async def on_tool_start(
+        self,
+        serialized: dict[str, Any],
+        input_str: str,
+        *,
+        run_id: UUID,
+        parent_run_id: Optional[UUID] = None,
+        tags: Optional[list[str]] = None,
+        metadata: Optional[dict[str, Any]] = None,
+        **kwargs: Any,
+    ) -> None:
+        """Send action to user, and persist to history.
+        This can be done in 'on_agent_action' as well, but I left it here to pair with 'on_tool_end'
+        """
+        action = {"tool_name": serialized["name"], "tool_input": input_str}
+        action_str = json.dumps(action)
+        message = ChatMessage(
+            id=run_id,
+            conversation=self.conversation_id,
+            from_="system",
+            content=action_str,
+            type="text",
+        )
+        await self.websocket.send_text(message.model_dump_json())
+        self.history.add_message(SystemMessage(content=action_str))
+
+    async def on_tool_end(
+        self,
+        output: str,
+        *,
+        run_id: UUID,
+        parent_run_id: Optional[UUID] = None,
+        tags: Optional[list[str]] = None,
+        **kwargs: Any,
+    ) -> None:
+        """Send 'observation' (action result) to user, and persist to history."""
+        message = ChatMessage(
+            id=run_id,
+            conversation=self.conversation_id,
+            from_="system",
+            content=output,
+            type="text",
+        )
+        await self.websocket.send_text(message.model_dump_json())
+        self.history.add_message(SystemMessage(content=output))
+
+    async def on_tool_error(
+        self,
+        error: BaseException,
+        *,
+        run_id: UUID,
+        parent_run_id: Optional[UUID] = None,
+        tags: Optional[list[str]] = None,
+        **kwargs: Any,
+    ) -> None:
+        message = ChatMessage(
+            id=run_id,
+            conversation=self.conversation_id,
+            from_="system",
+            content=str(error),
+            type="text",
+        )
+        await self.websocket.send_text(message.model_dump_json())
+        self.history.add_message(SystemMessage(content=str(error)))
