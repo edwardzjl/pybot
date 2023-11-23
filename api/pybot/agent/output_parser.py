c1dd8965f029f42c85365cac61a2063e68020d6f
Junlin Zhou <jlzhou@zjuici.com>
2023-11-23 09:48:11 +0800

    refactor: better output parser

--- api/pybot/agent/output_parser.py
+++ api/pybot/agent/output_parser.py
@@ -1,56 +1,53 @@
-import json
-import re
-from typing import Union
+import ast
+from typing import Generator, Union
 
 from langchain.agents import AgentOutputParser
 from langchain.schema import AgentAction, AgentFinish
-from loguru import logger
+
+
+def find_dicts(s: str) -> Generator[tuple[dict, int], None, None]:
+    """find dicts in a string
+    Modified from <https://stackoverflow.com/a/51862122/6564721>
+
+    Args:
+        s (str): source string
+
+    Yields:
+        Generator[tuple[dict, int], None, None]: generates a tuple of dict and the index of the last char of the dict
+    """
+    stack = []  # a stack to keep track of the brackets
+    buffer = ""  # a buffer to store current tracking string
+    for i, ch in enumerate(s):
+        if ch == "{":
+            buffer += ch
+            stack.append(ch)
+        elif ch == "}":
+            stack.pop(-1)
+            buffer += ch
+            if not stack:
+                yield ast.literal_eval(buffer), i
+                buffer = ""
+        elif stack:
+            buffer += ch
 
 
 class JsonOutputParser(AgentOutputParser):
-    """Output parser that referenced langchain.agents.conversational_chat.ConvoOutputParser.
+    """Output parser that extracts all dicts in the output and try to parse them into actions.
+    Only the first valid action will be returned.
     The AgentOutputParser is a langchain.load.serializable.Serializable which is a pydantic v1 model in the time of writing.
     """
 
-    pattern = re.compile(
-        r"(?:```)?(?:json)?\n(\{\s*\"tool_name\":.*?})\n(?:```)?", re.DOTALL
-    )
     tool_name_key: str = "tool_name"
     tool_input_key: str = "tool_input"
 
     def parse(self, text: str) -> Union[AgentAction, AgentFinish]:
-        try:
-            md_snippet_match = self.pattern.search(text)
-            if md_snippet_match is None:
-                # no markdown snippet, try to parse as raw json string
-                action_str: str = text.strip()
-                # sometimes LLM will generate action with thoughts or halucinations, we will extract the action part
-                action_str = action_str[
-                    action_str.index("{") : action_str.rindex("}") + 1
-                ]
-                action: dict = json.loads(action_str, strict=False)
-                return AgentAction(
-                    action.get(self.tool_name_key),
-                    action.get(self.tool_input_key),
-                    text,
-                )
-            action: dict = json.loads(md_snippet_match.group(1).strip(), strict=False)
-            if isinstance(action, list):
-                logger.warning(
-                    f"Got multiple action responses: {action}, using only the first one."
-                )
-                action = action[0]
-            if self.tool_name_key in action and self.tool_input_key in action:
-                tool_name = action.get(self.tool_name_key)
-                tool_input = action.get(self.tool_input_key)
-                if tool_name == "Final Answer":  # TODO: final answer key
-                    return AgentFinish({"output": tool_input}, text)
-                return AgentAction(tool_name, tool_input, text)
-            else:
-                raise ValueError("Not a valid tool response")
-        except Exception as e:
-            logger.error(f"Could not parse LLM output: {text}, error: {str(e)}")
-            return AgentFinish({"output": text}, text)
+        dicts: list[dict] = list(find_dicts(text))
+        for _dict, i in dicts:
+            if self.tool_name_key in _dict:
+                tool_name = _dict.get(self.tool_name_key)
+                tool_input = _dict.get(self.tool_input_key, "")
+                return AgentAction(tool_name, tool_input, text[: i + 1])
+        return AgentFinish({"output": text}, text)
 
     @property
     def _type(self) -> str:
