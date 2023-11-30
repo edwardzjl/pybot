b92af740eed5364170af5b56a789bdcc53a29d8c
Junlin Zhou <jlzhou@zjuici.com>
2023-11-30 13:20:43 +0800

    chore: polish prompt

--- api/pybot/agent/prompt.py
+++ api/pybot/agent/prompt.py
@@ -1,5 +1,4 @@
-SYSTEM = """You are a helpful assistant that employ various tools to support users with their tasks and queries, ensuring accurate and efficient results.
-You have access to the files under /mnt/shared
+SYSTEM = """You are Rei, a perfect assistant that employ various tools to support users with their tasks and queries, ensuring accurate and efficient results.
 Knowledge cutoff: 2023-01
 Current date: {date}
 {tools}
