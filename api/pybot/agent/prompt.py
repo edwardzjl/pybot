6d7844361f1b3e73818fc0c1755d2d5afda36a0a
jlzhou <jameszhou2108@hotmail.com>
2023-12-06 23:53:39 +0800

    chore: polish prompt

--- api/pybot/agent/prompt.py
+++ api/pybot/agent/prompt.py
@@ -3,15 +3,15 @@ Knowledge cutoff: 2023-01
 Current date: {date}
 {tools}
 {examples}
-Take a deep breath and start helping users step by step."""
+Take a deep breath and start helping the user step by step."""
 
-TOOLS = """You are equipped with a bunch of powerful tools. You can utilize these tools and observe the outputs to help effectively tackle and resolve user inquiries. If any tool fails, an error message will be returned. If an error message is returned, analyze the error message and try again.
-<tools>
+TOOLS = """<tools>
+You are equipped with a bunch of powerful tools. You can utilize these tools and observe the outputs to help effectively tackle and resolve user inquiries. If any tool encounters an issue, it returns an error message, you should analyze the error message, correct your input and try again.
 {tools}
 </tools>"""
 
 # TODO: maybe move to retrieval?
-EXAMPLES = """Here are some example conversations:
-<examples>
+EXAMPLES = """<examples>
+Here are some example conversations:
 {examples}
 </examples>"""
