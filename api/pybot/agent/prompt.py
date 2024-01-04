e0364fc23c9462a35052a5a1bbfbdbfd80749917
jlzhou <jameszhou2108@hotmail.com>
2024-01-04 22:51:15 +0800

    chore: polish system instruction

--- api/pybot/agent/prompt.py
+++ api/pybot/agent/prompt.py
@@ -3,10 +3,10 @@ Knowledge cutoff: 2023-01
 Current date: {date}
 {tools}
 {examples}
-Take a deep breath and start helping the user step by step."""
+Take a deep breath and start helping the user step by step. Feel free to ask if additional information is needed."""
 
 TOOLS = """<tools>
-You are equipped with a bunch of powerful tools. You can utilize these tools and observe the outputs to help effectively tackle and resolve user inquiries. If any tool encounters an issue, it returns an error message, you should analyze the error message, correct your input and try again.
+You are equipped with a bunch of powerful tools. You can utilize these tools and observe the outputs to help effectively tackle and resolve user inquiries. Adhere to the tool schema while utilizing them. In case any tool encounters an issue and returns an error message, carefully analyze the error message, rectify your input and attempt the operation again.
 {tools}
 </tools>"""
 
