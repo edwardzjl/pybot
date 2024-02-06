a8984dfb78254ad3b6f845ebd7ac62b44277814a
Junlin Zhou <jlzhou@zjuici.com>
2024-02-06 16:24:52 +0800

    refactor: remove update conversation callback

--- api/pybot/callbacks/__init__.py
+++ api/pybot/callbacks/__init__.py
@@ -1,11 +1,9 @@
 from pybot.callbacks.action import AgentActionCallbackHandler
-from pybot.callbacks.conversation import UpdateConversationCallbackHandler
 from pybot.callbacks.streaming import StreamingLLMCallbackHandler
 from pybot.callbacks.tracing import TracingLLMCallbackHandler
 
 __all__ = [
     "AgentActionCallbackHandler",
-    "UpdateConversationCallbackHandler",
     "StreamingLLMCallbackHandler",
     "TracingLLMCallbackHandler",
 ]
