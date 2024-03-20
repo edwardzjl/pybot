fb35c7cd2e21f863d517c77ae0035176725f7b88
jlzhou <jameszhou2108@hotmail.com>
2024-03-20 21:54:18 +0800

    refactor: polish kernel execution routine

--- api/pybot/tools/python.py
+++ api/pybot/tools/python.py
@@ -45,28 +45,52 @@ class CodeSandbox(BaseTool):
         self, code: str, run_manager: Optional[CallbackManagerForToolRun] = None
     ) -> str:
         """Use the tool."""
+        payload = ExecutionRequest.of_code(code)
+        logger.debug(f"kernel execution payload: {payload.model_dump_json()}")
         kernel = self.kernel_manager.start_kernel()
         with self.kernel_manager.upgrade(str(kernel.id)) as websocket:
-            payload = ExecutionRequest.of_code(code)
-            logger.debug(f"kernel execution payload: {payload.model_dump_json()}")
             result = ""
             try:
                 websocket.send(payload.model_dump_json())
                 while message := websocket.recv(timeout=self.timeout):
                     logger.trace(f"kernel execution message: [{message}]")
                     response = ExecutionResponse.model_validate_json(message)
+                    if response.parent_header.msg_id != payload.header.msg_id:
+                        # This message does not belong to the current execution
+                        # As we break early once we get the result, this could happen
+                        logger.debug(
+                            f"Ignoring message of parent id {response.parent_header.msg_id} in request {payload.header.msg_id}"
+                        )
+                        continue
                     match response.msg_type:
-                        case "error":
-                            result = (
-                                f"{response.content.ename}: {response.content.evalue}"
-                            )
-                            break
+                        case "execute_input":
+                            # Ignore broadcast message
+                            # See <https://jupyter-client.readthedocs.io/en/latest/messaging.html#code-inputs>
+                            logger.trace("Ignoring broadcast execution input.")
+                        case "execute_reply":
+                            # See <https://jupyter-client.readthedocs.io/en/latest/messaging.html#execution-results>
+                            # error execution may have extra messages, for example a stream std error
+                            if response.content.status == "error":
+                                result = f"{response.content.ename}: {response.content.evalue}"
+                            # For status != "error" we may want to extract the result from "stream" or "execute_result"
                         case "execute_result":
+                            # See <https://jupyter-client.readthedocs.io/en/latest/messaging.html#id6>
                             result = response.content.data.text_plain
-                            break
+                        case "display_data":
+                            # TODO: We can persist images here and return the path
+                            # See <https://jupyter-client.readthedocs.io/en/latest/messaging.html#display-data>
+                            ...
                         case "stream":
-                            result = response.content.text
-                            break
+                            # 'stream' is treated as second-class citizen. If 'execute_result', 'display_data' or 'execute_reply.error' exists,
+                            # We ignore the 'stream' message. If only all other messages has nothing to display, we will use the 'stream' message.
+                            if not result:
+                                result = response.content.text
+                        case "status":
+                            if response.content.execution_state == "idle":
+                                # idle means the kernel has finished executing
+                                # TODO: there will be rare situations that the idle message is received before the execute_result message
+                                # See <https://github.com/jupyter-server/enterprise_gateway/blob/54c8e31d9b17418f35454b49db691d2ce5643c22/enterprise_gateway/client/gateway_client.py#L235C9-L235C9>
+                                break
                         case _:
                             # debug because we don't handle many message types like status
                             logger.debug(f"Unhandled message type: {response.msg_type}")
@@ -79,34 +103,48 @@ class CodeSandbox(BaseTool):
         self, code: str, run_manager: Optional[AsyncCallbackManagerForToolRun] = None
     ) -> str:
         """Use the tool asynchronously."""
+        payload = ExecutionRequest.of_code(code)
+        logger.debug(f"kernel execution payload: {payload.model_dump_json()}")
         kernel = await self.kernel_manager.astart_kernel()
         async with self.kernel_manager.aupgrade(str(kernel.id)) as websocket:
-            payload = ExecutionRequest.of_code(code)
-            logger.debug(f"kernel execution payload: {payload.model_dump_json()}")
             result = ""
             try:
                 await websocket.send(payload.model_dump_json())
                 while message := await asyncio.wait_for(
                     websocket.recv(), timeout=self.timeout
                 ):
-                    logger.debug(f"kernel execution message: [{message}]")
+                    logger.trace(f"kernel execution message: [{message}]")
                     response = ExecutionResponse.model_validate_json(message)
                     if response.parent_header.msg_id != payload.header.msg_id:
-                        # ignore messages not related to this request
-                        # should rarely happen, but in case there's some unprocessed messages from previous run
+                        # This message does not belong to the current execution
+                        # As we break early once we get the result, this could happen
                         logger.debug(
                             f"Ignoring message of parent id {response.parent_header.msg_id} in request {payload.header.msg_id}"
                         )
                         continue
                     match response.msg_type:
-                        case "error":
-                            result = (
-                                f"{response.content.ename}: {response.content.evalue}"
-                            )
+                        case "execute_input":
+                            # Ignore broadcast message
+                            # See <https://jupyter-client.readthedocs.io/en/latest/messaging.html#code-inputs>
+                            logger.trace("Ignoring broadcast execution input.")
+                        case "execute_reply":
+                            # See <https://jupyter-client.readthedocs.io/en/latest/messaging.html#execution-results>
+                            # error execution may have extra messages, for example a stream std error
+                            if response.content.status == "error":
+                                result = f"{response.content.ename}: {response.content.evalue}"
+                            # For status != "error" we may want to extract the result from "stream" or "execute_result"
                         case "execute_result":
+                            # See <https://jupyter-client.readthedocs.io/en/latest/messaging.html#id6>
                             result = response.content.data.text_plain
+                        case "display_data":
+                            # TODO: We can persist images here and return the path
+                            # See <https://jupyter-client.readthedocs.io/en/latest/messaging.html#display-data>
+                            ...
                         case "stream":
-                            result = response.content.text
+                            # 'stream' is treated as second-class citizen. If 'execute_result', 'display_data' or 'execute_reply.error' exists,
+                            # We ignore the 'stream' message. If only all other messages has nothing to display, we will use the 'stream' message.
+                            if not result:
+                                result = response.content.text
                         case "status":
                             if response.content.execution_state == "idle":
                                 # idle means the kernel has finished executing
